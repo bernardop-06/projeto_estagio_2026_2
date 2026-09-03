bd130b05dd59025dc3fd72a417b6b6e8cd32a5b7
Bernardo Pinho <bernardo@fplagro.com.br>
2026-09-02 21:14:14 -0300

    Adiciona login e protecao da rota do painel

--- chamados/urls.py
+++ chamados/urls.py
@@ -1,7 +1,16 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.pagina_publica, name="pagina_publica"),
     path("enviado/", views.chamado_enviado, name="chamado_enviado"),
+    path("painel/", views.painel, name="painel"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="chamados/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
